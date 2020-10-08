5ed37f087243a34e72bd84d0a8aeaedac1792e67
Michele Bonacina <michele.bonacina@gmail.com>
2020-10-08 15:26:31 +0200

    added base trackpoint

---
+++ src/Track/GpxTrackpoint.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MicheleBonacina\PhpGpxLib\Waypoint;
+
+use MicheleBonacina\PhpGpxLib\Point\GpxPoint;
+
+/**
+ * GPX Trackpoint.
+ */
+class GpxTrackpoint extends GpxPoint
+{
+
+    public $timestamp;      // trackpoint timestamp
+    public $heartRate;      // trackpoint heart rate
+    public $cadence;        // trackpoint cadence
+
+    /**
+     * Creates a new trackpoint.
+     *
+     * @param $latitude waypoint latitude
+     * @param $longitude waypoint longitude
+     * @param $altitude waypoint altitude
+     * @param $timestamp waypoint timestamp
+     * @param $heartRate waypoint heart rate
+     * @param $cadence waypoint cadence
+     */
+    function __construct($latitude, $longitude, $altitude, $timestamp, $heartRate, $cadence)
+    {
+        parent::__construct($latitude, $longitude, $altitude);
+        $this->timestamp = $timestamp;
+        $this->heartRate = $heartRate;
+        $this->cadence = $cadence;
+    }
+}
