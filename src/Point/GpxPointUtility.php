58c2360f889e179a484b014b7f8a3a0a978eb6d9
Michele Bonacina <michele.bonacina@gmail.com>
2020-10-13 14:49:43 +0200

    added ascent and grade

--- src/Point/GpxPointUtility.php
+++ src/Point/GpxPointUtility.php
@@ -12,8 +12,11 @@ class GpxPointUtility
 
     /**
      * Calculates the distance in meters between two points.
+     * The distance consider geodetic distance, calculated using points latitue
+     * and longitude coordinates, and altitude difference (ascent).
+     * Distance is always returned as an absolute value.
      *
-     * @param GpxPoint $point1 forst point
+     * @param GpxPoint $point1 first point
      * @param GpxPoint $point2 second point
      * @return float distance in meters
      */
@@ -32,8 +35,47 @@ class GpxPointUtility
         // calculate geodetic distance
         $geodeticDistance = $arc * GpxPointUtility::EARTH_RADIUS;
         // calculate distance
-        $distance = sqrt(pow($geodeticDistance, 2) + pow($point1->getAltitude() - $point2->getAltitude(), 2));
+        $distance = sqrt(pow($geodeticDistance, 2) + pow($this->ascent($point1, $point2), 2));
         // return disance
         return $distance;
+    }
+
+    /**
+     * Calculates the ascent in meters between two points.
+     * The ascent is the difference between the second point altitude and
+     * the first point altitude. If greater then zero, the second point is
+     * higher than first, the opposite if lower then zero.
+     *
+     * @param GpxPoint $point1 first point
+     * @param GpxPoint $point2 second point
+     * @return float ascent in meters
+     */
+    public function ascent(GpxPoint $point1, GpxPoint $point2): float
+    {
+        // calculate ascent
+        $ascent = $point2->getAltitude() - $point1->getAltitude();
+        // return ascent
+        return $ascent;
+    }
+
+    /**
+     * Calculates the perncentage grade between two points.
+     * When horizontal and vertical distance between two points are the same
+     * the inclination is 45 degrees and the percentage grade is 100%.
+     * If greater then zero, the second point is higher than first, the opposite
+     * if lower then zero.
+     *
+     * @param GpxPoint $point1 first point
+     * @param GpxPoint $point2 second point
+     * @return float percentage grade
+     */
+    public function percentageGrade(GpxPoint $point1, GpxPoint $point2): float
+    {
+        // calculate ascent
+        $rise = $this->ascent($point1, $point2);
+        $run = sqrt(pow($this->distance($point1, $point2), 2) - pow($rise, 2));
+        $ascent = $rise / $run * 100;
+        // return ascent
+        return $ascent;
     }
 }
--- tests/Point/GpxPointUtilityTest.php
+++ tests/Point/GpxPointUtilityTest.php
@@ -11,23 +11,11 @@ class GpxPointUtilityTest extends TestCase
 {
 
     /**
-     * Test distance function.
-     *
-     * @dataProvider distanceProvider
-     */
-    public function testDistance(GpxPoint $point1, GpxPoint $point2): void
-    {
-        $pointUtility = new GpxPointUtility();
-        var_dump($pointUtility->distance($point1, $point2));
-        $this->assertTrue(TRUE);
-    }
-
-    /**
-     * Data provider for distance test.
+     * Data provider for testing two point operations.
      *
      * @return array test case
      */
-    public function distanceProvider(): array
+    public function twoPointProvider(): array
     {
         // load data from file
         $fileUtility = new GpxFileUtility();
@@ -41,5 +29,41 @@ class GpxPointUtilityTest extends TestCase
             [$waypoints[0], $waypoints[1]],
             [$trackPoints[0], $trackPoints[sizeof($trackPoints) - 1]]
         ];
+    }
+
+    /**
+     * Test distance function.
+     *
+     * @dataProvider twoPointProvider
+     */
+    public function testDistance(GpxPoint $point1, GpxPoint $point2): void
+    {
+        $pointUtility = new GpxPointUtility();
+        var_dump($pointUtility->distance($point1, $point2));
+        $this->assertTrue(TRUE);
+    }
+
+    /**
+     * Test distance function.
+     *
+     * @dataProvider twoPointProvider
+     */
+    public function testAscent(GpxPoint $point1, GpxPoint $point2): void
+    {
+        $pointUtility = new GpxPointUtility();
+        var_dump($pointUtility->ascent($point1, $point2));
+        $this->assertTrue(TRUE);
+    }
+
+    /**
+     * Test percentage grade.
+     *
+     * @dataProvider twoPointProvider
+     */
+    public function testPercentageGrade(GpxPoint $point1, GpxPoint $point2): void
+    {
+        $pointUtility = new GpxPointUtility();
+        var_dump($pointUtility->percentageGrade($point1, $point2));
+        $this->assertTrue(TRUE);
     }
 }
