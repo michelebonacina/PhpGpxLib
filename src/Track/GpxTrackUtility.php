d1f4d4f1d4a66ce954919288bbceb54b951d42bd
Michele Bonacina <michele.bonacina@gmail.com>
2020-10-13 16:48:33 +0200

    added delta temperature

--- src/Track/GpxTrackUtility.php
+++ src/Track/GpxTrackUtility.php
@@ -11,7 +11,7 @@ class GpxTrackUtility extends GpxPointUtility
 {
 
     /**
-     * Calculates the duration between two point.
+     * Calculates the duration between two points.
      * The duration is the time difference between two points in seconds.
      *
      * @param GpxPoint $point1 first point
@@ -24,5 +24,23 @@ class GpxTrackUtility extends GpxPointUtility
         $duration = $point2->getTimestamp()->getTimestamp() - $point1->getTimestamp()->getTimestamp();
         // return duration
         return $duration;
+    }
+
+    /**
+     * Calculates the difference between the temperature of two points.
+     * The delta is calculated between the second and the first point.
+     * If the delta i greater than zero the temperature increases,
+     * otherwise decreases.
+     *
+     * @param GpxPoint $point1 first point
+     * @param GpxPoint $point2 second point
+     * @return float temperature difference
+     */
+    public function deltaTemperature(GpxTrackPoint $point1, GpxTrackPoint $point2): float
+    {
+        // calculate the delta temperature
+        $delta = $point2->getTemperature() - $point1->getTemperature();
+        // return delta
+        return $delta;
     }
 }
--- tests/Track/GpxTrackUtilityTest.php
+++ tests/Track/GpxTrackUtilityTest.php
@@ -11,7 +11,7 @@ class GpxTrackUtilityTest extends TestCase
 {
 
     /**
-     * Data provider for testing two point operations.
+     * Data provider for testing duration.
      *
      * @return array test case
      */
@@ -37,5 +37,34 @@ class GpxTrackUtilityTest extends TestCase
     {
         $trackUtility = new GpxTrackUtility();
         $this->assertEquals($expected, $trackUtility->duration($point1, $point2), "Wrong point duration");
+    }
+
+    /**
+     * Data provider for testing delta temperature.
+     *
+     * @return array test case
+     */
+    public function deltaTemperatureProvider(): array
+    {
+        // load data from file
+        $fileUtility = new GpxFileUtility();
+        $gpx = $fileUtility->loadTrackFromFile("C:\\Temp\\test.gpx");
+        // get track points
+        $trackPoints = $gpx->listTracks()[0]->listTrackSegments()[0]->listTrackPoints();
+        // return test data
+        return [
+            [$trackPoints[0], $trackPoints[sizeof($trackPoints) - 1], 2.0]
+        ];
+    }
+
+    /**
+     * Test distance function.
+     *
+     * @dataProvider deltaTemperatureProvider
+     */
+    public function testDeltaTemperature(GpxTrackPoint $point1, GpxTrackPoint $point2, float $expected): void
+    {
+        $trackUtility = new GpxTrackUtility();
+        $this->assertEquals($expected, $trackUtility->deltaTemperature($point1, $point2), "Wrong delta temperature");
     }
 }
