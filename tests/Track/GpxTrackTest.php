516985d8e28ccc71e35f25c708bff9316f3ca3d3
Michele Bonacina <michele.bonacina@gmail.com>
2020-10-21 17:51:17 +0200

    added total ascent and descent to track

--- tests/Track/GpxTrackTest.php
+++ tests/Track/GpxTrackTest.php
@@ -37,4 +37,60 @@ class GpxTrackTest extends TestCase
     {
         $this->assertEquals($expected, $track->duration(), "Wrong track duration");
     }
+
+    /**
+     * Data provider for testing total ascent.
+     *
+     * @return array test case
+     */
+    public function totalAscentProvider(): array
+    {
+        // load data from file
+        $fileUtility = new GpxFileUtility();
+        $gpx = $fileUtility->loadTrackFromFile("C:\\Temp\\test.gpx");
+        // get track points
+        $track = $gpx->listTracks()[0];
+        // return test data
+        return [
+            [$track, 561.4]
+        ];
+    }
+
+    /**
+     * Test distance function.
+     *
+     * @dataProvider totalAscentProvider
+     */
+    public function testTotalAscent(GpxTrack $track, float $expected): void
+    {
+        $this->assertEquals($expected, $track->totalAscent(), "Wrong track total ascent");
+    }
+
+    /**
+     * Data provider for testing total descnt.
+     *
+     * @return array test case
+     */
+    public function totalDescentProvider(): array
+    {
+        // load data from file
+        $fileUtility = new GpxFileUtility();
+        $gpx = $fileUtility->loadTrackFromFile("C:\\Temp\\test.gpx");
+        // get track points
+        $track = $gpx->listTracks()[0];
+        // return test data
+        return [
+            [$track, 77.2]
+        ];
+    }
+
+    /**
+     * Test total descent function.
+     *
+     * @dataProvider totalDescentProvider
+     */
+    public function testTotalDescent(GpxTrack $track, float $expected): void
+    {
+        $this->assertEquals($expected, $track->totalDescent(), "Wrong track total descent");
+    }
 }
